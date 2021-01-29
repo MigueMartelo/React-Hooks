5470728961ed34b49289e8b6b89c3e70a89bf969
Miguel Martelo <miguemartelo1031@gmail.com>
2021-01-28 21:58:38 -0500

    Integración de pagos con la API de PayPal

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,12 +1,72 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { PayPalButton } from 'react-paypal-button';
+import AppContext from '../context/AppContext';
 import '../styles/components/Payment.css';
 
 const Payment = () => {
+  const history = useHistory();
+
+  const {
+    state: { cart, buyer },
+    addNewOrder,
+  } = useContext(AppContext);
+
+  const paypalOptions = {
+    clientId:
+      'AX5xk8TlOhsFQY-30S0LtCkoqjbga6USuhFd7YG_fb6HvXSWdfD2YDwOfpkz_wzaoYvKGR96FBNTqs_4',
+    intent: 'capture',
+    currency: 'USD',
+  };
+
+  const buttonStyles = {
+    layout: 'vertical',
+    shape: 'rect',
+  };
+
+  const handlePaymentSuccess = (data) => {
+    console.log(data);
+    if (data.status === 'COMPLETED') {
+      const newOrder = {
+        buyer,
+        products: cart,
+        payment: data,
+      };
+      addNewOrder(newOrder);
+      history('/checkout/success');
+    }
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
+
   return (
     <div className="Payment">
       <div className="Payment-content">
         <h3>Resumen del pedido</h3>
-        <div className="Payment-button">Botón de pago con Paypal</div>
+        {cart.map((item) => (
+          <div className="Payment-item" key={item.title}>
+            <div className="Payment-element">
+              <h4>{item.title}</h4>
+              <span>{`$ ${item.price}`}</span>
+            </div>
+          </div>
+        ))}
+        <div className="Payment-button">
+          <PayPalButton
+            paypalOptions={paypalOptions}
+            buttonStyles={buttonStyles}
+            amount={handleSumTotal()}
+            onPaymentStart={() => console.log('Start Payment')}
+            onPaymentSuccess={(data) => handlePaymentSuccess(data)}
+            onPaymentError={(error) => console.log(error)}
+            onPaymentCancel={(data) => console.log(data)}
+          />
+        </div>
       </div>
       <div>Sidebar proximo</div>
     </div>
